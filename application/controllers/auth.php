e0f6483a1591ad18107b9c9f537045cdfa9d9da6
yudhawahyup27 <wyudha104@gmail.com>
2022-04-19 11:48:49 +0700

    menambah rules dan function constract

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -3,6 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class auth extends CI_Controller
 {
+
+    public function __construct(){
+    Parent::__construct();
+    $this->load->library('form_validation');
+    }
     public function index()
     {
         $data['$title'] = "Rumah Pintar Login";
@@ -13,9 +18,18 @@ class auth extends CI_Controller
 
     public function register()
     {
-        $data['$title'] = "Rumah Pintar Register";
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/register');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('name','Name', 'required|alpha_numeric|trim');
+        $this->form_validation->set_rules('email','Email', 'required|valid_email|trim');
+        // validasi Jika salah memasukan
+        if ($this->form_validation->run() == false ) {
+            $data['$title'] = "Rumah Pintar Register";
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/register');
+            $this->load->view('templates/auth_footer');
+        } else {
+            echo 'Data Ditambahkan';
+        }
+
+        
     }
 }
