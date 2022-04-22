8ea0e52f83c0d9e002d19d22c696e597ecd2ae68
yudha <wyudha104@gmail.com>
2022-04-22 09:37:15 +0700

    menambah index halaman homepage

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -4,13 +4,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class auth extends CI_Controller
 {
 
-    public function __construct(){
-    Parent::__construct();
-    $this->load->library('form_validation');
-    }
-    public function index()
+    public function __construct()
     {
-        $data[$title] = "Rumah Pintar Login";
+        Parent::__construct();
+        $this->load->library('form_validation');
+    }
+    public function login()
+    {
+        $data['$title'] = "Rumah Pintar Login";
         $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
@@ -18,10 +19,10 @@ class auth extends CI_Controller
 
     public function register()
     {
-        $this->form_validation->set_rules('name','Name', 'required|alpha_numeric|trim');
-        $this->form_validation->set_rules('email','Email', 'required|valid_email|trim');
+        $this->form_validation->set_rules('name', 'Name', 'required|alpha_numeric|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|trim');
         // validasi Jika salah memasukan
-        if ($this->form_validation->run() == false ) {
+        if ($this->form_validation->run() == false) {
             $data['$title'] = "Rumah Pintar Register";
             $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/register');
@@ -29,7 +30,5 @@ class auth extends CI_Controller
         } else {
             echo 'Data Ditambahkan';
         }
-
-        
     }
 }
---
+++ application/controllers/index.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class index extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('Home/index');
+    }
+}
